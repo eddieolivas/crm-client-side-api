647f2bb650c0f23fd5cf7d82bc8c1ce0d0e39f10
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-09-22 19:51:40 -0700

    Started login route.

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const { insertUser } = require("../models/user/User.model");
 const { hashPassword } = require("../helpers/bcrypt.helper");
+const { json } = require("express");
 
 router.all("/", (req, res, next) => {
   //res.json({ message: "Response from user router." });
   next();
 });
 
+// Create new user router
 router.post("/", async (req, res) => {
   const { name, company, address, phone, email, password } = req.body;
 
@@ -32,6 +34,10 @@ router.post("/", async (req, res) => {
   } catch (error) {
     res.json({ status: "error", message: error.message });
   }
+});
+// User sign in router
+router.post("/login", (req, res) => {
+  res.json({ status: "success", message: "Login successful" });
 });
 
 module.exports = router;
